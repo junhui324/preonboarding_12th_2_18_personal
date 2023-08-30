071aed0a61e1f1d66ffe57d4f52b4088322cb2a6
junhui324 <junhuijang00@gmail.com>
2023-08-30 17:23:29 +0900

    fix: 5번째 이슈 대신 광고 출력 문제 해결 기존 로직 : 5번째 이슈 대신 광고 출력 문제점 : 매 5번째 이슈를 출력할 수 없음 해결 : 이슈와 광고를 한꺼번에 비교하던 로직 대신, 광고만 비교하는 로직 사용      : ex)index > 0 && index % 4 === 0 && (광고 출력) 문제점2 : 위와 같이 해결했더니 무한 스크롤이 되지 않는 문제 발생 해결 : React.Fragment로 광고 및 이슈를 래핑 (불필요한 DOM 노드 생성 방지)      : 래핑 이후, 기존에 광고에 주었던 observer를 없애고 맨 마지막에 따로 요소 생성 후 observer ref를 줘서 무한 스크롤 기능 동작하도록 함

--- src/components/issueListForm/IssueListForm.tsx
+++ src/components/issueListForm/IssueListForm.tsx
@@ -2,18 +2,19 @@ import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { getIssueList } from '../../api/IssueApi';
-import LoadingSpinner from '../loading/Loading';
+//import LoadingSpinner from '../loading/Loading';
 
 import styles from './IssueListForm.module.scss';
 import AdImage from '../../assets/adimage.png';
 import ROUTES from '../../utils/constants/Routes';
+import React from 'react';
 
 export default function IssueListForm() {
-	const [issues, setIssues] = useState([]);
+	const [issues, setIssues] = useState<any>([]);
 	const [currentPage, setCurrentPage] = useState(1);
-	const [isLoading, setIsLoading] = useState(false);
+	//const [isLoading, setIsLoading] = useState(false);
 
-	const observerRef = useRef(null);
+	const observerRef = useRef<any>(null);
 
 	const navigate = useNavigate();
 
@@ -22,7 +23,7 @@ export default function IssueListForm() {
 	}, []);
 
 	const getIssueListData = async () => {
-		setIsLoading(true);
+		//setIsLoading(true);
 		try {
 			const res = await getIssueList(currentPage);
 			// @ts-ignore
@@ -30,17 +31,13 @@ export default function IssueListForm() {
 			setCurrentPage(prevPage => prevPage + 1);
 		} catch (err) {
 			console.error(err);
-		} finally {
-			setIsLoading(false);
 		}
 	};
 
 	const lastElementObserver = (node: HTMLElement | null) => {
 		if (observerRef.current) {
-			// @ts-ignore
 			observerRef.current.disconnect();
 		}
-		// @ts-ignore
 		observerRef.current = new IntersectionObserver(entries => {
 			if (entries[0].isIntersecting) {
 				getIssueListData();
@@ -48,7 +45,6 @@ export default function IssueListForm() {
 		});
 
 		if (node) {
-			// @ts-ignore
 			observerRef.current.observe(node);
 		}
 	};
@@ -64,46 +60,35 @@ export default function IssueListForm() {
 	return (
 		<div className={styles.container}>
 			<h1>이슈 리스트</h1>
-			{isLoading ? (
-				<LoadingSpinner />
-			) : (
-				<ul>
-					{issues.map((issue: any, index) =>
-						(index + 1) % 5 === 0 ? (
-							<li key={index} className={styles.adBox}>
-								{index === issues.length - 1 ? (
-									<div ref={lastElementObserver}>
-										<button className={styles.adImageBtn} onClick={handleClickAdImage}>
-											<img className={styles.img} src={AdImage} alt="advertisementImage"></img>
-										</button>
-									</div>
-								) : (
-									<button className={styles.adImageBtn} onClick={handleClickAdImage}>
-										<img className={styles.img} src={AdImage} alt="advertisementImage"></img>
-									</button>
-								)}
-
-								<div className={styles.line}></div>
-							</li>
-						) : (
-							<li key={index} className={styles.issueBox}>
-								<button onClick={() => handleClickIssue(issue.number)}>
-									<div className={styles.issueTitle}>
-										<span>#{issue.number}</span>
-										<span>{issue.title}</span>
-									</div>
-									<div className={styles.issueDescription}>
-										<span>작성자 : {issue.user.login}</span>
-										<span>작성일 : {issue.created_at}</span>
-										<span>코멘트 : {issue.comments}</span>
-									</div>
+			<ul>
+				{issues.map((item: any, index: number) => (
+					<React.Fragment key={index}>
+						{index > 0 && index % 4 === 0 && (
+							<li className={styles.adBox}>
+								<button className={styles.adImageBtn} onClick={handleClickAdImage}>
+									<img className={styles.img} src={AdImage} alt="advertisementImage" />
 								</button>
 								<div className={styles.line}></div>
 							</li>
-						),
-					)}
-				</ul>
-			)}
+						)}
+						<li className={styles.issueBox}>
+							<button onClick={() => handleClickIssue(item.number)}>
+								<div className={styles.issueTitle}>
+									<span>#{item.number}</span>
+									<span>{item.title}</span>
+								</div>
+								<div className={styles.issueDescription}>
+									<span>작성자 : {item.user.login}</span>
+									<span>작성일 : {item.created_at}</span>
+									<span>코멘트 : {item.comments}</span>
+								</div>
+							</button>
+							<div className={styles.line}></div>
+						</li>
+					</React.Fragment>
+				))}
+			</ul>
+			<div ref={lastElementObserver}></div>
 		</div>
 	);
 }
